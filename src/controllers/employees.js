fe8bea0c24242760849bf1d215dff54474626c5b
Ravinder Yadav <ravinderyadav092007@gmail.com>
2026-02-27 13:58:22 +0530

    Update employees controller

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -4,56 +4,28 @@ const Employee = require("../models/employees");
 // ADD EMPLOYEE
 const addEmployee = async (req, res) => {
   try {
+    const { name, password, qualification, institute, department, mobileNumber, emergencyContact, email, dob, address, startTime, endTime, salary, accountNumber, ifscCode, bankName, bankHolderName} = req.body;
 
-    const {
-      name,
-      password,
-      qualification,
-      institute,
-      department,
-      mobileNumber,
-      emergencyContact,
-      email,
-      dob,
-      address,
-      startTime,
-      endTime,
-      salary,
-      accountNumber,
-      ifscCode,
-      bankName,
-      bankHolderName
-    } = req.body;
+    if (
+      !name || !password || !email || !mobileNumber || !department || !salary) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all required fields"
+      });
+    }
 
     const existingEmployee = await Employee.findOne({ email });
 
     if (existingEmployee) {
       return res.status(400).json({
+        success: false,
         message: "Employee already exists"
       });
     }
 
-    const employee = new Employee({
-      name,
-      password,
-      qualification,
-      institute,
-      department,
-      mobileNumber,
-      emergencyContact,
-      email,
-      dob,
-      address,
-      startTime,
-      endTime,
-      salary,
-      accountNumber,
-      ifscCode,
-      bankName,
-      bankHolderName
+    const employee = await Employee.create({
+      name, password, qualification, institute, department, mobileNumber, emergencyContact, email, dob, address, startTime, endTime, salary, accountNumber, ifscCode, bankName, bankHolderName
     });
-
-    await employee.save();
 
     res.status(201).json({
       success: true,
@@ -72,6 +44,4 @@ const addEmployee = async (req, res) => {
   }
 };
 
-module.exports = {
-  addEmployee
-};
+module.exports = {addEmployee };
