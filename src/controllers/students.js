16b4ec222d669cc06463bb0830c8f22ac0a115a0
Tushar <sainitushar322@gmail.com>
2026-03-10 14:06:59 +0530

    CRUD APIs Created

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -1,9 +1,17 @@
+const bcrypt = require("bcrypt");
 const { Students } = require("../modals/students");
 
-const add_student = async (req, res) => {
+
+const add_Student = async (req, res) => {
     try {
         const { course_id, batch_id, enrollment_id, name, father_name, mother_name, address, aadhar, pan_card, emi, contact, email, password, dob } = await req.body;
-        
+        const existing = await Students.findOne({ email });
+
+        if (existing) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
         const data = await Students.create({
             course_id,
             batch_id,
@@ -17,15 +25,78 @@ const add_student = async (req, res) => {
             emi,
             contact,
             email,
-            password,
+            password: hashedPassword,
             dob
         })
 
         return res.status(201).json({ message: "Data added Successfully.", data })
     } catch (error) {
-        return res.status(500).json({ message: error.message })
         console.log(error.message);
+        return res.status(500).json({ message: error.message })
     }
 }
 
-module.exports = { add_student }
+
+const getAll_Student = async (req, res) => {
+    try {
+        const data = await Students.find();
+
+        return res.status(200).json({ message: "Data fetched Successfully.", data })
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ message: error.message })
+    }
+}
+
+
+const getOne_Student = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const data = await Students.findById(id);
+        if (!data) {
+            return res.status(404).json({ message: "Data not found." });
+        }
+
+        return res.status(200).json({ message: "Data fetched Successfully.", data })
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ message: error.message })
+    }
+}
+
+
+const update_Student = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const updated_Data = await Students.findByIdAndUpdate(id, req.body, { new: true, runValidators: true })
+        if (!updated_Data) {
+            return res.status(404).json({ message: "Data not found." });
+        }
+
+        return res.status(200).json({ message: "Data updated Successfully.", updated_Data })
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ message: error.message })
+    }
+}
+
+
+const delete_Student = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deleted_Data = await Students.findByIdAndDelete(id)
+        if (!deleted_Data) {
+            return res.status(404).json({ message: "Data not found." });
+        }
+
+        return res.status(200).json({ message: "Data deleted Successfully.", deleted_Data })
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({ message: error.message })
+    }
+}
+
+
+module.exports = { add_Student, getAll_Student, getOne_Student, update_Student, delete_Student }
