507e02c12d159c38a01ef43dcd8113fe482e6bc2
Tushar <sainitushar322@gmail.com>
2026-03-31 16:53:41 +0530

    Student Section Completed

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-
 const connectDb = async () => {
     mongoose.connect("mongodb://localhost:27017/tipsg-alwar-backend").then(() => {
         console.log("Connected to MongoDB");
