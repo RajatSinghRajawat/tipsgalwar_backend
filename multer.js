39a966762b0835f2e6d8756b56727007ccfea9e3
Mayant1108 <mayant.tipsgalwar@gmail.com>
2026-04-01 12:17:04 +0530

    Batch Section Completed

--- multer.js
+++ multer.js
@@ -1,24 +1,11 @@
 const multer = require("multer");
 const path = require("path");
-const fs = require("fs");
-
-const uploadDir = path.join(__dirname, "src", "public", "Uploads");
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 const storage = multer.diskStorage({
-<<<<<<< HEAD
     destination: function (req, file, cb) {
         cb(null, "public/Uploads");
     },
     filename: function (req, file, cb) {
-=======
-  destination: function (req, file, cb) {
-    cb(null, uploadDir);
-  },
-  filename: function (req, file, cb) {
->>>>>>> origin/ravinder
         cb(null, Date.now() + " " + file.originalname);
     },
 });
